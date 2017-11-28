448c7af201a97ea1e07a29f64493839818433296
thaivankhoa <thaivankhoa2802@gmail.com>
2017-11-28 16:07:55 +0700

    youtube-api-search bar, video list, video list item, video detail

--- src/index.js
+++ src/index.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAUfBcQDhqt-6sIJaaKTap9SQu36QfFZJ0';
-// Create a new component
-//
-const App = () => {
-	return <div> Hi! </div>;
+
+
+
+// Create a new component. This component should produce
+//some html
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null };
+
+		YTSearch({key: API_KEY, term: 'macbook pro 2017' }, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			 });
+		});
+	}
+	render() {
+		return(
+			<div>
+				<SearchBar />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo: selectedVideo}) } 
+					videos={this.state.videos} />
+			</div>
+		);
+	}
 }
 
 
